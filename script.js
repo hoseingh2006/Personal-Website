48c36f880edc9f9262169359ae148b60d4ee467c
hoseingh2006 <H03einGhasemi2006@gmail.com>
2025-03-22 01:30:34 +0330

    animation card project

--- script.js
+++ script.js
@@ -26,3 +26,42 @@ const observer = new IntersectionObserver((entries) => {
 contents.forEach((content) => {
     observer.observe(content);
 });
+
+
+// ////////////////////////////////////
+
+
+let cards = document.querySelectorAll('.section-Projects-card')
+
+cards.forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        // موقعیت موس نسبت به مرکز کارت
+        const rect = card.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+
+        const deltaX = (mouseX - centerX) / (rect.width / 2);
+        const deltaY = (mouseY - centerY) / (rect.height / 2);
+
+
+        card.style.transform = `
+            rotateY(${deltaX * 15}deg)
+            rotateX(${-deltaY * 15}deg)
+        `;
+
+        const innerElement = card.querySelector('.inner-element');
+        innerElement.style.transform = `
+            translateZ(100px) /* بیرون‌زدگی بیشتر */
+            scale(${1 + Math.abs(deltaX) * 0.3}, ${1 + Math.abs(deltaY) * 0.3})
+        `;
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+        const innerElement = card.querySelector('.inner-element');
+        innerElement.style.transform = 'translateZ(30px) scale(1, 1)';
+    });
+});
